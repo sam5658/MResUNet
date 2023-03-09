82c86b0f302f8248111e95de37c3a114310212a8
Kian <101457916+sam5658@users.noreply.github.com>
2023-03-09 16:26:56 +0530

    Update mresunet.py

--- mresunet.py
+++ mresunet.py
@@ -373,7 +373,7 @@ class MResUNet(pl.LightningModule):
                 d2=d2_list[self.nb_enc_boxes - 2 - i],
             )
           
-        
+        print("hgfgdg")
         mass = self.final_layers(x)
         print(mass.shape , "sdgf")
         return mass.view((mass.shape[0], 1, 1, 1))
